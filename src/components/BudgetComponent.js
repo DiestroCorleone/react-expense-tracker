267660b240a204b38b829458f5be82128a33ca63
DiestroCorp <matias.perez.mcd@gmail.com>
2022-09-25 00:16:09 -0300

    Added cursor pointer to Budget pill.

--- src/components/BudgetComponent.js
+++ src/components/BudgetComponent.js
@@ -3,7 +3,7 @@ export default function BudgetComponent(props) {
     <div
       className={`border rounded-pill col p-4 m-1 text-white text-center fw-bold ${
         props.budgetType === "Budget"
-          ? "bg-success bg-gradient"
+          ? "bg-success bg-gradient cursor-pointer"
           : props.budgetType === "Remaining"
           ? "bg-warning bg-gradient"
           : props.budgetType === "Spent" && "bg-danger bg-gradient"
